f34aa29699d31c1b5301b1d823d13c5509f23317
dheenadhayalanperumal <dheena1992@gmail.com>
2025-09-24 16:25:06 +0530

    Add gradient background and update layout with new header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { AppBar, Toolbar, Button, Box } from '@mui/material';
+import logo from '../assets/images/logo.png'; // Importing logo from asset
+
+// import { Image } from '@mui/icons-material'; // Importing Image component
+
+const Header: React.FC = () => {
+  return (
+    <AppBar position="static" sx={{ margin: 0, padding: 0, backgroundColor: 'transparent', boxShadow: 'none' }}>
+      <Toolbar sx={{ padding: '0 !important', margin: 0, minHeight: 'auto !important' }}>
+        <Box sx={{ display: 'flex', alignItems: 'center', flexGrow: 1, padding: 0, margin: 0 }}>
+          <img src={logo.src} alt="Logo" style={{ width: '168px', height: '42px' }} />
+        </Box>
+        <Button variant="outlined" sx={{ width: '101', height: '42px',margin: 0, padding: '4px 8px', color: 'inherit', borderColor: 'currentColor',borderRadius:'5px' }}>
+          Login
+        </Button>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
